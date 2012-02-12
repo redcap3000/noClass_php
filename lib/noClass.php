a0e2507366dacf8b4216d0b667d800ed3530ae26
Ronaldo Barbachano <ronaldo@redcapmedia.com>
2012-02-11 20:15:14 -0600

    removed example stuff from noclass(reorganized)

--- lib/noClass.php
+++ lib/noClass.php
@@ -1,10 +1,6 @@
 <?php
 require('couchCurl.php');
 require('htmler.php');
-$time = microtime(); 
-$time = explode(" ", $time); 
-$time = $time[1] + $time[0]; 
-$start = $time; 
 
 /*
 	noClass framework
@@ -298,68 +294,5 @@ class noClass_html{
 	}
 }
 	
-class blog extends noClass_html{
-
-	public $_f = array('_id','post_title','category','post_type','post_tags','publisher','summary','content','status');
-	public $_r = array('_id','post_title','post_type','publisher','content','status');
-	public $post_title;
-
-	public $category;
-	public $parent_category;
-	public $content = 'textarea ';
-
-	public $post_tags;
-	public $thumbnail_image;
-	public $front_image;
-	public $parent_post;
-	public $post_type = array('select:post_type '=> array('post'=>'Post','page'=>'Page'));
-	public $publisher;
-	public $summary = 'textarea ';
-	public $published = 'date ';
-	public $created;
-	public $last_edit;
-	public $status = array('select:status'=> array('draft'=>'Draft','published'=>'Published','private'=>'Private'  ) );
 
 
-	public function __toString(){
-		$this->title .= $this->post_title;
-		return parent::__toString();
-	}
-}
-
-
-// Here is a container for the blog. This structure can easily be stored in as json and modified
-// and reloaded. Keys other than 'head' 'body' are either method calls for HTMLER (anything with
-// a double underscore) and values are strings, keys that are present in the object (non htmler calls)
-// are formatted according to the paired value (which is a htmler call)
-
-
-$container = array(	'head' => 	array('link__stylesheet'=>'main.css') ,
-							'body'=> 	array('div__page'=> 
-												array('div__post' =>
-													array( 'post_title'=> 'h2__' , 'category'=>'h3__','publisher'=>'h4__','post' => 'div__','post_tags'=> 'div__' ) 
-													) 
-												)	 
-							);
-
-$blog = new blog($container);
-
-// change invoke to allow a 'container' ? if container is_array 
-// then load the template ? ?
-
-
-echo $blog();
-
-//$blog = apc_fetch('blog');
-// simple one line APC check/setter
-//!$blog && $blog = new blog AND apc_add('blog',$blog);
-
-//echo $blog('test');
-
-$time = microtime(); 
-$time = explode(" ", $time); 
-$time = $time[1] + $time[0]; 
-$finish = $time; 
-$totaltime = (round($finish - $start,3)  * 1000); 
-
-echo '<div id="stats">'.round((memory_get_usage() / 1024),1) . "(k) $totaltime (ms)</div>";
